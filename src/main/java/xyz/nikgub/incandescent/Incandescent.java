4e22108d165161ba1327ec4d63e5593d21233784
nikgub_ <danger8217.nik@gmail.com>
2024-02-06 18:04:37 +0300

    Added client tick counter

--- src/main/java/xyz/nikgub/incandescent/Incandescent.java
+++ src/main/java/xyz/nikgub/incandescent/Incandescent.java
@@ -1,13 +1,30 @@
+/*
+    Incandescent Lib, Minecraft Forge light-weight library
+    Copyright (C) 2024, nikgub_
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package xyz.nikgub.incandescent;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -19,6 +36,7 @@ public class Incandescent
     public static final String MOD_ID = "incandescent_lib";
 
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static int clientTick;
 
     public Incandescent()
     {
@@ -29,6 +47,11 @@ public class Incandescent
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
+    }
+
+    private void clientTick(final TickEvent.ClientTickEvent event)
+    {
+        clientTick++;
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
