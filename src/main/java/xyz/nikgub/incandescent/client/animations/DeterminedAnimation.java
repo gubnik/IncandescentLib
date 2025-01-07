e28d462079200cdbd42fe40715390b79bd1d75e1
nikgub_ <danger8217.nik@gmail.com>
2025-01-07 18:15:46 +0300

    Provided default serializer for AnimationPurpose Extended and reordered AnimationPurpose enum Removed deprecated fields

--- src/main/java/xyz/nikgub/incandescent/client/animations/DeterminedAnimation.java
+++ src/main/java/xyz/nikgub/incandescent/client/animations/DeterminedAnimation.java
@@ -1,31 +1,42 @@
 package xyz.nikgub.incandescent.client.animations;
 
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.world.entity.AnimationState;
 
 /**
  * Record used by {@link IAnimationPurposeEntity}
  * @param animationState            AnimationState of an entity
  * @param animationPurpose          Type of animation, must be declared properly for generalized behaviour
- * @param signal                    Byte value of a signal used by Entity.handleEntityEvent(byte)
- * @param localPriority             Animations with higher priority are prioritized, also acts as an id.<p>
- *
- * @since 1.2.0                     byte and localPriority params are deprecated and will be removed in 1.3.0
  */
-public record DeterminedAnimation(AnimationState animationState, AnimationPurpose animationPurpose, byte signal, int localPriority)
+public record DeterminedAnimation(AnimationState animationState, AnimationPurpose animationPurpose)
 {
+    @SuppressWarnings("unused")
     public enum AnimationPurpose
     {
+        IDLE,
         MAIN_ATTACK,
         SPECIAL_ATTACK,
+        ULTIMATE_ATTACK,
         HURT,
         SPECIAL_HURT,
         SPAWN,
         DEATH,
-        MISC,
-        IDLE,
+        SPECIAL_DEATH,
         STOMP,
         DIG,
         CONJURE,
-        TRANSFORM
+        TRANSFORM,
+        MISC_1,
+        MISC_2,
+        MISC_3,
+        MISC_4,
+    }
+
+    public static final EntityDataSerializer<AnimationPurpose> ANIMATION_SERIALIZER = EntityDataSerializer.simpleEnum(DeterminedAnimation.AnimationPurpose.class);
+
+    static
+    {
+        EntityDataSerializers.registerSerializer(ANIMATION_SERIALIZER);
     }
 }
