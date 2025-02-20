527b6a786e2def24cd3fb27bf45eb62fac8b38ab
nikgub_ <danger8217.nik@gmail.com>
2025-02-20 22:16:22 +0300

    Change the constructor to allow free resource access

--- src/main/java/xyz/nikgub/incandescent/itemgen/ItemGenConfigProvider.java
+++ src/main/java/xyz/nikgub/incandescent/itemgen/ItemGenConfigProvider.java
@@ -6,6 +6,7 @@ import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
@@ -21,18 +22,56 @@ public class ItemGenConfigProvider
 {
     private final Map<String, ItemGenObjectInfo> itemObjects;
 
-    public ItemGenConfigProvider (String loc)
+    private ItemGenConfigProvider (final String jsonString)
     {
         Gson gson = new Gson();
+        Type type = TypeToken.getParameterized(Map.class, String.class, TypeToken.getParameterized(Map.class, String.class, Object.class).getType()).getType();
+        Map<String, Map<String, Object>> map = gson.fromJson(jsonString, type);
+        itemObjects = map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, stringMapEntry -> new ItemGenObjectInfo(stringMapEntry.getValue())));
+    }
+
+    public static ItemGenConfigProvider inAnyDir (String rawLocation)
+    {
+        final String jsonString;
         try
         {
-            Type type = TypeToken.getParameterized(Map.class, String.class, TypeToken.getParameterized(Map.class, String.class, Object.class).getType()).getType();
-            Map<String, Map<String, Object>> map = gson.fromJson(Files.readString(Path.of(FMLPaths.CONFIGDIR.toString(),"config", loc)), type);
-            itemObjects = map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, stringMapEntry -> new ItemGenObjectInfo(stringMapEntry.getValue())));
+            jsonString = Files.readString(Path.of(rawLocation));
         } catch (IOException e)
         {
             throw new RuntimeException(e);
         }
+        return new ItemGenConfigProvider(jsonString);
+    }
+
+    public static ItemGenConfigProvider inConfigDir (String configLocation)
+    {
+        final String jsonString;
+        try
+        {
+            jsonString = Files.readString(Path.of(FMLPaths.CONFIGDIR.toString(),"config", configLocation));
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return new ItemGenConfigProvider(jsonString);
+    }
+
+    public static ItemGenConfigProvider inDataDir (String dataLocation)
+    {
+        final String jsonString;
+        final URL url = ItemGenConfigProvider.class.getClassLoader().getResource(dataLocation);
+        if (url == null)
+        {
+            throw new RuntimeException("Unable to open file \"" + dataLocation + "\" in data folder");
+        }
+        try
+        {
+            jsonString = Files.readString(Path.of(url.getPath()));
+        } catch (IOException e)
+        {
+            throw new RuntimeException("Unable to open file \"" + dataLocation + "\" in data folder", e);
+        }
+        return new ItemGenConfigProvider(jsonString);
     }
 
     public Map<String, ItemGenObjectInfo> getItemObjects ()
