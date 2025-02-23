f940497d1305882840028d4f63faf8b7d442b401
nikgub_ <danger8217.nik@gmail.com>
2025-02-23 03:31:13 +0300

    Fix bad config definition

--- src/main/java/xyz/nikgub/incandescent/IncandescentConfig.java
+++ src/main/java/xyz/nikgub/incandescent/IncandescentConfig.java
@@ -34,13 +34,13 @@ public class IncandescentConfig
         .comment("Defines how severe is the screenshake")
         .defineInRange("screenshake_amount", 0.01d, 0, 1f);
 
-    public static final ForgeConfigSpec.ConfigValue<Boolean> COMMON_ALLOW_FORCED_ENTITY_NBT_SYNC = SERVER_BUILDER
+    public static final ForgeConfigSpec.ConfigValue<Boolean> COMMON_ALLOW_FORCED_ENTITY_NBT_SYNC = COMMON_BUILDER
         .comment("Set true if you want to allow to sync entity NBTs via SyncEntityNBTEvent. This will not override server config.")
-        .define("server_allow_forced_entity_nbt_sync", false);
+        .define("allow_forced_entity_nbt_sync", false);
 
     public static final ForgeConfigSpec.ConfigValue<Boolean> SERVER_ALLOW_FORCED_ENTITY_NBT_SYNC = SERVER_BUILDER
         .comment("Set true if you want to allow to sync entity NBTs via SyncEntityNBTEvent on the server")
-        .define("server_allow_forced_entity_nbt_sync", false);
+        .define("allow_forced_entity_nbt_sync", false);
 
     static final ForgeConfigSpec COMMON_SPEC = COMMON_BUILDER.build();
     static final ForgeConfigSpec SERVER_SPEC = SERVER_BUILDER.build();
